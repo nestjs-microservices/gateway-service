1acac068ba8264482bf36626ecf61ee92b30eab9
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-02-14 01:10:42 +0300

    feat: fetching listening port from env variables

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { PresentationModule } from './presentation/presentation.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(PresentationModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('GATEWAY_PORT'));
 }
 bootstrap();
