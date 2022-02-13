d63f5659493e62c8a46e8516c831757a48cebeae
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-02-14 00:44:00 +0300

    feat: implementing get user by id use case

---
+++ src/application/use-cases/get-user-by-email/get-user-by-email.use-case.ts
@@ -0,0 +1,14 @@
+import { UseCase } from '../base.use-case';
+import { ClientProxy } from '@nestjs/microservices';
+import { Inject, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class GetUserByEmailUseCase implements UseCase {
+  constructor(
+    @Inject('USER_SERVICE') private readonly clientProxy: ClientProxy,
+  ) {}
+
+  exec(payload) {
+    return this.clientProxy.send({ cmd: 'getUserByEmail' }, payload);
+  }
+}
