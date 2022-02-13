17b9de601b8b338d0fef4430a65f0a3cf4ef983e
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-02-14 01:27:34 +0300

    feat: replacing hardcoded value by env variables

--- src/presentation/controllers/health.controller.ts
+++ src/presentation/controllers/health.controller.ts
@@ -5,12 +5,14 @@ import {
   MicroserviceHealthIndicator,
 } from '@nestjs/terminus';
 import { Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('health')
 export class HealthController {
   constructor(
     private readonly healthCheckService: HealthCheckService,
     private microServiceHealthIndicator: MicroserviceHealthIndicator,
+    private configService: ConfigService,
   ) {}
 
   @Get()
@@ -20,7 +22,10 @@ export class HealthController {
       async () =>
         this.microServiceHealthIndicator.pingCheck('tcp', {
           transport: Transport.TCP,
-          options: { host: '0.0.0.0', port: 8888 },
+          options: {
+            host: this.configService.get('USER_SERVICE_HOST'),
+            port: this.configService.get('USER_SERVICE_PORT'),
+          },
         }),
     ]);
   }
