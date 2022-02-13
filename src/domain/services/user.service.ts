d32e4ee850f0172bbfe294c685fab603da0c3dac
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-02-13 16:56:49 +0300

    fix: implementing get user service

--- src/domain/services/user.service.ts
+++ src/domain/services/user.service.ts
@@ -6,7 +6,12 @@ export class UserService {
   constructor(
     @Inject('USER_SERVICE') private readonly userProxy: ClientProxy,
   ) {}
+
   createUser(args) {
     return this.userProxy.send({ cmd: 'createUser' }, args);
+  }
+
+  getUser(email: string) {
+    return this.userProxy.send({ cmd: 'getUser' }, email);
   }
 }
