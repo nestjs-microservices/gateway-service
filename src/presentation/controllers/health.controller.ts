0a68287ba72b6c83c3b79b65b9d1fe10fd04f413
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-02-14 01:05:05 +0300

    feat: add health endpoint

--- src/presentation/controllers/health.controller.ts
+++ src/presentation/controllers/health.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  MicroserviceHealthIndicator,
+} from '@nestjs/terminus';
+import { Transport } from '@nestjs/microservices';
 
 @Controller('health')
-export class HealthController {}
+export class HealthController {
+  constructor(
+    private readonly healthCheckService: HealthCheckService,
+    private microServiceHealthIndicator: MicroserviceHealthIndicator,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.healthCheckService.check([
+      async () =>
+        this.microServiceHealthIndicator.pingCheck('tcp', {
+          transport: Transport.TCP,
+          options: { host: '0.0.0.0', port: 8888 },
+        }),
+    ]);
+  }
+}
