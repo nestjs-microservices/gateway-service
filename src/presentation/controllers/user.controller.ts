7f7ef355b891ba463c2341e0af0e8a7d20f14e67
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-02-14 00:49:17 +0300

    feat: replacing user service by use cases

--- src/presentation/controllers/user.controller.ts
+++ src/presentation/controllers/user.controller.ts
@@ -1,13 +1,15 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { UserService } from '../../domain/services/user.service';
 import { CreateUserUseCase } from '../../application/use-cases/create-user/create-user.use-case';
+import { GetUserByIdUseCase } from '../../application/use-cases/get-user-by-id/get-user-by-id.use-case';
+import { GetUserByEmailUseCase } from '../../application/use-cases/get-user-by-email/get-user-by-email.use-case';
 import { CreateUserPayload } from '../../application/use-cases/create-user/create-user.payload';
 
 @Controller('users')
 export class UserController {
   constructor(
-    private readonly userService: UserService,
     private readonly createUserUseCase: CreateUserUseCase,
+    private readonly getUserByIdUseCase: GetUserByIdUseCase,
+    private readonly getUserByEmailUseCase: GetUserByEmailUseCase,
   ) {}
 
   @Post()
@@ -17,11 +19,11 @@ export class UserController {
 
   @Get('email/:email')
   getUser(@Param('email') email: string) {
-    return this.userService.getUserByEmail(email);
+    return this.getUserByEmailUseCase.exec(email);
   }
 
   @Get(':id')
   getUserById(@Param('id') id: number) {
-    return this.userService.getUserById(id);
+    return this.getUserByIdUseCase.exec(id);
   }
 }
