bfaa9a6f8fdfddae9cabbb650d676a147a186f55
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-02-15 02:06:25 +0300

    feat: adding update user endpoint

--- src/presentation/controllers/user.controller.ts
+++ src/presentation/controllers/user.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { CreateUserUseCase } from '../../application/use-cases/create-user/create-user.use-case';
 import { GetUserByIdUseCase } from '../../application/use-cases/get-user-by-id/get-user-by-id.use-case';
 import { GetUserByEmailUseCase } from '../../application/use-cases/get-user-by-email/get-user-by-email.use-case';
 import { CreateUserPayload } from '../../application/use-cases/create-user/create-user.payload';
+import { UpdateUserUseCase } from '../../application/use-cases/update-user/update-user.use-case';
+import { UpdateUserPayload } from '../../application/use-cases/update-user/update-user.payload';
 
 @Controller('users')
 export class UserController {
@@ -10,6 +12,7 @@ export class UserController {
     private readonly createUserUseCase: CreateUserUseCase,
     private readonly getUserByIdUseCase: GetUserByIdUseCase,
     private readonly getUserByEmailUseCase: GetUserByEmailUseCase,
+    private readonly updateUserUseCase: UpdateUserUseCase,
   ) {}
 
   @Post()
@@ -25,5 +28,10 @@ export class UserController {
   @Get(':id')
   getUserById(@Param('id') id: number) {
     return this.getUserByIdUseCase.exec(id);
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: number, @Body() data: UpdateUserPayload) {
+    return this.getUserByIdUseCase.exec({ id, data });
   }
 }
